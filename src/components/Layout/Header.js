b83fec44dfe222a185bd6cf411355195a75d24bd
ShrutikaLondhe15 <shrutikalondhe15@gmail.com>
2024-03-16 01:03:57 +0530

    Building Headers(t1)

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,19 @@
+import React, { Fragment } from "react";
+import classes from './Header.module.css'
+
+import mealsimg from '../../assets/meals.jpg';
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = (props) =>{
+    return <Fragment>
+        <header className={classes.header}>
+            <h1>MyRestro</h1>
+            <HeaderCartButton/>
+        </header>
+        <div className={classes['main-image']}>
+            <img src={mealsimg} alt="A Table full of Delicious Food!!"/>
+        </div>
+    </Fragment>
+}
+
+export default Header;
